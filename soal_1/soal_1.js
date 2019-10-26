41772d3f8d8f8718cd3699af2e9a697c81b8944b
afdhalluthfi <afdhalluthfi145@gmail.com>
2019-10-26 16:38:06 +0700

    soal_1 selesai

--- soal_1/soal_1.js
+++ soal_1/soal_1.js
@@ -1,1 +1,25 @@
 console.log('hello')
+
+var dataKey =["dumb","ways","the","best"]
+var x;
+var word = 'dumbwaysthebest'
+console.log(word.length)
+
+
+
+ken =  word.slice(0,-11)
+ken2 =  word.slice(4,-7)
+console.log(ken+' '+ken2)
+
+
+function chek(dataKey, word){
+    if(dataKey[0] == word.slice(0,-11) && dataKey[1] == word.slice(4,-7)){
+        console.log(dataKey[0]+'=>'+Boolean(dataKey[0]))
+        console.log(dataKey[1]+'=>'+Boolean(dataKey[1]))
+
+    }else(
+        console.log('salah')
+    )
+}
+
+chek(dataKey,word)
