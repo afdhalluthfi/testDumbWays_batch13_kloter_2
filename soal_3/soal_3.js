09dec68c53d97b8c83e724111e98661691709e06
afdhalluthfi <afdhalluthfi145@gmail.com>
2019-10-26 19:01:34 +0700

    hasil_soal3

--- soal_3/soal_3.js
+++ soal_3/soal_3.js
@@ -1,10 +1,22 @@
-var lebar = 10;
-var panjang = 5;
-
+var lebar = 8;
+var panjang = 4;
 for(var i =0; i <= panjang; i++){
-    for(var j = 0; j <= lebar; j++){
-       
-        document.write("*");
+    if(i % 2){
+
+        for(var j = 0; j <= lebar; j++){
+            if(j % 4){
+                
+                document.write("=");
+            }else{
+                document.write("*");
+            }
+           
+        }
+    }else{
+        for(j =0; j<= lebar; j++){
+            document.write("*");
+    
+        }
     }
     document.write("</br>");
 }
