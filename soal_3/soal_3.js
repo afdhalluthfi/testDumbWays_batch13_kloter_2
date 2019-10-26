41fd86cd60d40b19123083332d1e76cc1e923d9c
afdhalluthfi <afdhalluthfi145@gmail.com>
2019-10-26 18:31:02 +0700

    soal_3

---
+++ soal_3/soal_3.js
@@ -0,0 +1,10 @@
+var lebar = 10;
+var panjang = 5;
+
+for(var i =0; i <= panjang; i++){
+    for(var j = 0; j <= lebar; j++){
+       
+        document.write("*");
+    }
+    document.write("</br>");
+}
